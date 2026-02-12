0f8bb47ff09efc287c00b0b1b0820b378121fb7b
Aryan <cse200001010@iiti.ac.in>
2026-02-12 23:45:54 +0530

    Tanstack router initial Setup

--- discord-clone-frontend/vite.config.ts
+++ discord-clone-frontend/vite.config.ts
@@ -1,10 +1,17 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
