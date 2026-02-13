c52fd638d3bef984f389dc0d93e2eb29e6d7a47d
Aryan <cse200001010@iiti.ac.in>
2026-02-13 16:05:18 +0530

    Remaining tanstack router integration

--- discord-clone-frontend/src/main.tsx
+++ discord-clone-frontend/src/main.tsx
@@ -1,13 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
--- discord-clone-frontend/src/routes/__root.tsx
+++ discord-clone-frontend/src/routes/__root.tsx
@@ -1,15 +1,22 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import * as React from "react";
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <React.Fragment>
       <div>Hello "__root"!</div>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>
+      </div>
       <Outlet />
+      <TanStackRouterDevtools />
     </React.Fragment>
-  )
+  );
 }
