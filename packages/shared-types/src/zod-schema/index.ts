aa0e12aa748230f17f42e1c07dd3b3afc18cfac6
Aryan <cse200001010@iiti.ac.in>
2026-03-06 23:50:48 +0530

    User Initial Auth DB Integration

--- packages/shared-types/src/zod-schema/index.ts
+++ packages/shared-types/src/zod-schema/index.ts
@@ -4,6 +4,7 @@ export const UserRegisterationSchema = z.object({
   username: z.string().min(3).max(20),
   name: z.string().min(3).max(20),
   password: z.string().min(8).max(32),
+  email: z.string().email(),
 });
 
 export const UserLoginSchema = z.object({
