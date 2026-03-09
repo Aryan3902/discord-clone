9671cf23deab5af9fb1e62ee21830c89c5ef35ba
Aryan <cse200001010@iiti.ac.in>
2026-03-09 23:19:52 +0530

    Login and register UI

--- apps/discord-clone-frontend/src/routeTree.gen.ts
+++ apps/discord-clone-frontend/src/routeTree.gen.ts
@@ -9,10 +9,16 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
+import { Route as RegisterRouteImport } from './routes/register'
 import { Route as LoginRouteImport } from './routes/login'
 import { Route as ChannelsRouteImport } from './routes/channels'
 import { Route as IndexRouteImport } from './routes/index'
 
+const RegisterRoute = RegisterRouteImport.update({
+  id: '/register',
+  path: '/register',
+  getParentRoute: () => rootRouteImport,
+} as any)
 const LoginRoute = LoginRouteImport.update({
   id: '/login',
   path: '/login',
@@ -33,34 +39,45 @@ export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
   '/channels': typeof ChannelsRoute
   '/login': typeof LoginRoute
+  '/register': typeof RegisterRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
   '/channels': typeof ChannelsRoute
   '/login': typeof LoginRoute
+  '/register': typeof RegisterRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
   '/channels': typeof ChannelsRoute
   '/login': typeof LoginRoute
+  '/register': typeof RegisterRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/channels' | '/login'
+  fullPaths: '/' | '/channels' | '/login' | '/register'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/channels' | '/login'
-  id: '__root__' | '/' | '/channels' | '/login'
+  to: '/' | '/channels' | '/login' | '/register'
+  id: '__root__' | '/' | '/channels' | '/login' | '/register'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
   ChannelsRoute: typeof ChannelsRoute
   LoginRoute: typeof LoginRoute
+  RegisterRoute: typeof RegisterRoute
 }
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
+    '/register': {
+      id: '/register'
+      path: '/register'
+      fullPath: '/register'
+      preLoaderRoute: typeof RegisterRouteImport
+      parentRoute: typeof rootRouteImport
+    }
     '/login': {
       id: '/login'
       path: '/login'
@@ -89,6 +106,7 @@ const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   ChannelsRoute: ChannelsRoute,
   LoginRoute: LoginRoute,
+  RegisterRoute: RegisterRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
--- apps/discord-clone-frontend/src/routes/register.tsx
+++ apps/discord-clone-frontend/src/routes/register.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/login")({
+export const Route = createFileRoute("/register")({
   component: RouteComponent,
 });
 
@@ -9,13 +9,26 @@ function RouteComponent() {
     <div className="flex items-center justify-center h-screen">
       <div className="bg-zinc-800 w-[40rem] rounded-xl p-10">
         <div className="text-center">
-          <h3 className="text-3xl font-bold my-2">Welcome back!</h3>
-          <p className="text-md text-zinc-200">
-            We're so excited to have you back!
-          </p>
+          <h3 className="text-3xl font-bold my-2">Create an account!</h3>
         </div>
         <div>
           <form className="flex flex-col gap-6 ">
+            <div className="flex flex-col gap-2">
+              <label className="text-white font-normal text-lg">Email</label>
+              <input
+                type="email"
+                className="bg-zinc-900 rounded-md p-2 font-normal text-lg"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label className="text-white font-normal text-lg">
+                Display Name
+              </label>
+              <input
+                type="text"
+                className="bg-zinc-900 rounded-md p-2 font-normal text-lg"
+              />
+            </div>
             <div className="flex flex-col gap-2">
               <label className="text-white font-normal text-lg">Username</label>
               <input
@@ -34,13 +47,13 @@ function RouteComponent() {
               type="submit"
               className="bg-blue-500 rounded-md p-2 font-medium text-xl hover:bg-blue-600 transition-colors"
             >
-              Login
+              Register
             </button>
           </form>
           <p className="text-zinc-200 text-sm my-2">
-            Don't have an account?{" "}
-            <Link to="/register" className="text-blue-300 hover:text-blue-600">
-              Register
+            Already have an account?{" "}
+            <Link to="/login" className="text-blue-300 hover:text-blue-600">
+              Login
             </Link>
           </p>
         </div>
