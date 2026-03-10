92dbe030b53fc80a48a5bd24a06079c955698c98
Aryan <cse200001010@iiti.ac.in>
2026-03-10 12:33:14 +0530

    Express Backend Cors

--- apps/discord-clone-backend/src/index.ts
+++ apps/discord-clone-backend/src/index.ts
@@ -3,9 +3,16 @@ import authRoutes from "./api/v1/auth/routes.js";
 import cookieParser from "cookie-parser";
 import { env } from "./config/env.js";
 import { connectRedis } from "./config/redis.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser()); // Add this middleware BEFORE routes
 
