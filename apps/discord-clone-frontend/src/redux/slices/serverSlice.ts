06cd67af17f08b22e5445c7d8b9447ec08ca7bad
Aryan <cse200001010@iiti.ac.in>
2026-04-23 20:25:13 +0530

    Removing channel and server state from redux

--- apps/discord-clone-frontend/src/redux/slices/serverSlice.ts
+++ apps/discord-clone-frontend/src/redux/slices/serverSlice.ts
@@ -1,22 +1,15 @@
 import { getServers } from "@/api/getServers";
-import { getSelectedChannelId } from "@/lib/utils";
 import { ServerDetails } from "@/types/server";
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 interface ServerState {
   servers: ServerDetails[];
-  selectedServerId: number | null;
-  selectedChannelIds: SelectedChannelIdsInterface;
   loading: boolean;
   error: string | null;
 }
 
 const initialState: ServerState = {
   servers: [], // Array of all servers the user has joined
-  selectedServerId: localStorage.getItem("lastSelectedServerId")
-    ? Number(localStorage.getItem("lastSelectedServerId"))
-    : null, // The server the user has selected
-  selectedChannelIds: {}, // Map of channels the user has selected for each server
   error: null,
   loading: true,
 };
@@ -24,31 +17,11 @@ const initialState: ServerState = {
 const serverSlice = createSlice({
   name: "server",
   initialState,
-  reducers: {
-    selectServer: (state, action: PayloadAction<number>) => {
-      state.selectedServerId = action.payload;
-      localStorage.setItem("lastSelectedServerId", action.payload.toString());
-    },
-    selectChannel: (
-      state,
-      action: PayloadAction<{ serverId: number; channelId?: number | null }>
-    ) => {
-      let selectedChannelId = action.payload.channelId;
-      // In case the user is visiting a channel for the first time
-      if (!selectedChannelId)
-        selectedChannelId = getSelectedChannelId(
-          action.payload.serverId,
-          state.selectedChannelIds,
-          state.servers
-        );
-      state.selectedChannelIds[action.payload.serverId] = selectedChannelId;
-    },
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchServersAsync.fulfilled, (state, action) => {
         state.servers = action.payload;
-        state.selectedServerId = action.payload[0].id;
         state.loading = false;
       })
       .addCase(fetchServersAsync.pending, (state) => {
@@ -78,11 +51,10 @@ export const fetchServersAsync = createAsyncThunk(
       return x;
     } catch (error: unknown) {
       return rejectWithValue(
-        `Failed to sign up. Please try again. Error: ${error}`
+        `Failed to sign up. Please try again. Error: ${error}`,
       );
     }
-  }
+  },
 );
 
-export const { selectServer, selectChannel } = serverSlice.actions;
 export default serverSlice.reducer;
