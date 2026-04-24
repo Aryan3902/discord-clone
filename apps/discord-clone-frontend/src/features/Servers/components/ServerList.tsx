ede36909494d184a6cda8c8eef1467fc49cae616
Aryan <cse200001010@iiti.ac.in>
2026-04-24 23:08:25 +0530

    Add new Server Icon

--- apps/discord-clone-frontend/src/features/Servers/components/ServerList.tsx
+++ apps/discord-clone-frontend/src/features/Servers/components/ServerList.tsx
@@ -1,6 +1,7 @@
 import ServerIcon from "./ServerIcon";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
+import { PlusIcon } from "lucide-react";
 
 const ServerList = () => {
   const servers = useSelector((state: RootState) => state.server.servers);
@@ -9,6 +10,11 @@ const ServerList = () => {
       {servers.map((server) => (
         <ServerIcon serverDetails={server} key={server.id} />
       ))}
+      <div className="px-4 p-2">
+        <div className="size-10 bg-zinc-700 rounded-xl flex items-center justify-center cursor-pointer hover:bg-blue-500 transition-colors duration-300">
+          <PlusIcon className="size-6 text-white" />
+        </div>
+      </div>
     </div>
   );
 };
